78fd5428f62016fad794b8bee346c4f813d71411
4015d17 <4015d17@chironja.ece.uprm.edu>
2017-02-08 14:25:59 -0400

    Changed default window size from 400x100 to 200x200. Changed the name of the application window from "Hello World" to "Christopher Roman".

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Christopher Roman");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(850, 450);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		
+		//Application window's location based on computer monitor screen used in the laboratory.
 	}
 }
